f732b67beaf5ca52e4251f34acb2866edbe80b1f
KaguraRinko <wuxiaofei31@gmail.com>
2023-12-22 20:31:21 -0300

    PIHooks: Don't spoof Google One  * It'll break Google One VPN sometimes.
    Change-Id: I56aba92bfe6550bfb01d3253538f6aa566c497f6
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -58,7 +58,6 @@ public class PropImitationHooks {
     private static final String PACKAGE_PIXELWALLPAPER = "com.google.android.apps.wallpaper.pixel";
     private static final String PACKAGE_TURBO = "com.google.android.apps.turbo";
 
-    private static final String PACKAGE_SUBSCRIPTION_RED = "com.google.android.apps.subscriptions.red";
     private static final String PACKAGE_WALLPAPER = "com.google.android.apps.wallpaper";
     private static final String PACKAGE_WALLPAPEREFFECTS = "com.google.android.wallpaper.effects";
 
@@ -163,7 +162,6 @@ public class PropImitationHooks {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
         } else if (sSpoofGapps && (packageName.equals(PACKAGE_VELVET)
-		|| packageName.equals(PACKAGE_SUBSCRIPTION_RED)
 		|| packageName.equals(PACKAGE_TURBO)
                 || packageName.equals(PACKAGE_ASI)
 		|| packageName.equals(PACKAGE_GBOARD)
